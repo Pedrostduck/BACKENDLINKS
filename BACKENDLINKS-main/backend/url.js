6346639e0eb57606a56e35735a7a6323a8b7a974
Pedrostduck <160145564+Pedrostduck@users.noreply.github.com>
2025-06-05 09:18:06 -0300

    Delete BACKENDLINKS-main/backend directory

--- BACKENDLINKS-main/backend/url.js
+++
@@ -1,9 +0,0 @@
-const texto = `Trabalho escolar siga https://site.com
-Esse Link está invalido http://invalido..com
-Esse esta incompleto htp://erro
-Esse esta disponivel https://correto.org
-Veja Tambem http://blog.exemplo.org/artigo?tag=js.`;
-
-function extrairlinksvalidos(texto){
-    const possiveislinks = texto.match(/https?:\/\/[^\s]+/gi) || [];
-}
